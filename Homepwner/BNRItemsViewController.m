d345f6278e16963ab39d693655b7b03acc687837
Miguel A. Moreno <miguel@tyba.com>
2014-11-26 16:44:37 +0100

    implementation of some of the challenges

--- Homepwner/BNRItemsViewController.m
+++ Homepwner/BNRItemsViewController.m
@@ -32,15 +32,37 @@
            forCellReuseIdentifier:@"UITableViewCell"];
 }
 
--(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [[[BNRItemStore sharedStore] allItems] count];
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
 }
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSInteger count = 1;
+    if(section == 0){
+        count = [[[BNRItemStore sharedStore] allItems] count];
+    }
+    return count;
+}
+
+-(CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(indexPath.section == 0) {
+        return 60;
+    }else{
+        return 44;
+    }
+}
+
 -(UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell* cell = [self.tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
-
-    NSArray* items = [[BNRItemStore sharedStore] allItems];
-    BNRItem* item = items[indexPath.row];
-    cell.textLabel.text = [item description];
+    if( indexPath.section == 0) {
+        NSArray* items = [[BNRItemStore sharedStore] allItems];
+        BNRItem* item = items[indexPath.row];
+        cell.textLabel.text = [item description];
+        cell.backgroundColor = [UIColor lightGrayColor];
+        cell.textLabel.font = [UIFont systemFontOfSize:20];
+    } else {
+        cell.textLabel.text = @"No more items!";
+    }
     return cell;
 }
 @end
